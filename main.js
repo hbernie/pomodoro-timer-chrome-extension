aa8ee3ede64556dc7880f775b42b73e756da21f3
hbernie <hbern00@gmail.com>
2022-09-22 09:00:59 -0700

    Extension functionality complete, still wanting to add features

---
+++ main.js
@@ -0,0 +1,105 @@
+let ourTimer;
+let restartTimer;
+let timer;
+let sound = new Audio('sound.mp3');
+let working = true;
+
+function startTimer(duration, display, working) {
+  //populating everything to be duration
+  timer = duration; 
+  let minutes = duration;
+  let seconds = duration;
+
+  ourTimer = setInterval(function () {
+    //need parseInt so numbers aren't crazy
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+
+      //if minute or second is single digit add the 0 in front
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      //populate display
+      display.textContent = minutes + ":" + seconds;
+
+      //if timer decremented is less than 0 then stop timer
+      if (--timer < 0) stop(working);
+
+  }, 1000);
+  
+  function stop(working){
+    //stops the interval
+    clearInterval(ourTimer);
+    // readyForBreak();
+    if (working) {
+      working = false;
+      sound.play();
+      let title = document.getElementById("title");
+      title.innerHTML = "Break Time!";
+      startTimer(5, document.querySelector('#timerDisplay'), working);
+    } else if (!working) {
+      working = true;
+      sound.play();
+      let title = document.getElementById("title");
+      title.innerHTML = "Back to Work!";
+      startTimer(20, document.querySelector('#timerDisplay'), working);
+    }
+  }
+
+}
+
+document.getElementById('start').onclick = () => {
+  restartTimer = timer;
+  pause();
+  startTimer(restartTimer, document.querySelector('#timerDisplay'), working)
+};
+
+document.getElementById('pause').onclick = () => {
+  restartTimer = timer
+  pause()
+};
+
+function pause() {
+    clearInterval(ourTimer);
+}
+
+
+// Button pause -> Saves current state of timer <p> and converts it to seconds and save it to the variable time
+// Button start -> Takes the variable time a starts/restarts the timer by passing it to the start timer.
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  // When the button is clicked, inject setPageBackgroundColor into current page
+const hide = document.getElementById('hide');
+hide.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: hideElements(),
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function hideElements() {
+  chrome.storage.sync.get("*", function(obj){
+    document.body.style.visibility = hide;
+  });
+}
+  // STRETCH FEATURE - hide current page
+  // chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+  //   let url = tabs[0].url;
+  //   if (document.location.href.indexOf(url) != -1) {
+  //        $("*").hide();
+  //   }
+  let duration = 20, // your time in seconds here
+      display = document.querySelector('#timerDisplay');
+  startTimer(duration, display, working);
+});
+
+
+
+
+
+
