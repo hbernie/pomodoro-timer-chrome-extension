a6c85865fc1650390e96fd448dfe4f7f8c87c473
hbernie <hbern00@gmail.com>
2022-09-22 17:11:07 -0700

    Last commit before presentation

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ let restartTimer;
 let timer;
 let sound = new Audio('sound.mp3');
 let working = true;
+let hidden = false;
 
 function startTimer(duration, display, working) {
   //populating everything to be duration
   timer = duration; 
   let minutes = duration;
   let seconds = duration;
+
 
   ourTimer = setInterval(function () {
     //need parseInt so numbers aren't crazy
@@ -63,61 +65,26 @@ function pause() {
     clearInterval(ourTimer);
 }
 
-
-// Button pause -> Saves current state of timer <p> and converts it to seconds and save it to the variable time
-// Button start -> Takes the variable time a starts/restarts the timer by passing it to the start timer.
-
-
-document.addEventListener("DOMContentLoaded", function() {
-  // When the button is clicked, inject setPageBackgroundColor into current page
-// const hide = document.getElementById('hide');
-// hide.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     func: hideElements(),
-//   });
-// });
-
-// // The body of this function will be executed as a content script inside the
-// // current page
-// function hideElements() {
-//   chrome.storage.sync.get("*", function(obj){
-//     document.body.style.visibility = 'hide';
-//   });
-// }
-  // STRETCH FEATURE - hide current page
-  // chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-  //   let url = tabs[0].url;
-  //   if (document.location.href.indexOf(url) != -1) {
-  //        $("*").hide();
-  //   }
-  let duration = 20, // your time in seconds here
-      display = document.querySelector('#timerDisplay');
-  startTimer(duration, display, working);
-});
-
-
-hide.addEventListener("click", async () => {
+document.getElementById('hide').onclick = async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: hideElements(),
+    func: hidebutton(working)
   });
-});
+}
 
-// The body of this function will be executed as a content script inside the
-// current page
-function hideElements() {
-  const hide = document.getElementById('hide');
-  chrome.storage.sync.get("hide", function(obj){
-    obj.body.style.backgroundColor = 'yellow';
-  });
+function hidebutton() {
+  const content = document.querySelector('body');
+  content.style.display = 'none';
 }
 
 
 
+//extension starts here
+document.addEventListener("DOMContentLoaded", function() {
+  let duration = 20, // your time in seconds here
+      display = document.querySelector('#timerDisplay');
+  startTimer(duration, display, working);
 
-
+});
